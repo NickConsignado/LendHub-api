de85e4572751f48827177d2abc2698317d8f69b6
Nick Consignado <nickconsignado@gmail.com>
2023-08-09 15:23:14 +0800

    add admin and tags

--- database/migrations/2023_08_08_100916_create_tags_table.php
+++ database/migrations/2023_08_08_100916_create_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->enum('name', 'Romance', 'Drama', 'Comedy');
+            $table->enum('genre', ['Romance', 'Drama', 'Comedy']);
             $table->timestamps();
         });
     }
